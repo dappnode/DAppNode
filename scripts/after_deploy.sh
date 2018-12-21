bc1b4e3808db3af32b49d40e8d3638a81f7c4cd8
dapplion <35266934+dapplion@users.noreply.github.com>
2018-12-21 10:22:54 +0700

    Update after_deploy.sh

--- scripts/after_deploy.sh
+++ scripts/after_deploy.sh
@@ -20,18 +20,21 @@ echo "Running DAppNode travis CI after_deploy.sh script"
 git add "dappnode_package.json"
 git add "docker-compose.yml"
 #   Attempt the commit and push. In case of error (no changes), ignore
-git commit -m "Update manifest and docker-compose versions" && git push origin || echo "Files are already updated"
+git commit -m "Update manifest and docker-compose versions to current release: $TRAVIS_TAG" && git push origin || echo "Files are already updated"
 
 # 2. Create a new branch with the next version number
+# Check if the dappnodesdk is available
+dappnodesdk --help
 export FUTURE_VERSION=$(dappnodesdk increase patch)
 export FUTURE_VERSION=$(echo "$FUTURE_VERSION" | awk '/Next version:/{print $3}')
 export BRANCH_NAME="v${FUTURE_VERSION}"
+echo "Run dappnodesdk and increased version to FUTURE_VERSION: $FUTURE_VERSION"
 git checkout -b $BRANCH_NAME
 
 # 3. Commit the new*new versions of the docker-compose.yml and dappnode_package.json
 git add "dappnode_package.json"
 git add "docker-compose.yml"
-git commit -m "Advance manifest and docker-compose versions to new version"
+git commit -m "Advance manifest and docker-compose versions to new version: $FUTURE_VERSION"
 git push origin $BRANCH_NAME
 
 # 4. Open a pull request to master
