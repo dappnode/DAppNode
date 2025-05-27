d02e5a2a81d7bad5d6e030bb495cacf002c6d5e5
pablomendezroyo <mendez4a@gmail.com>
2025-05-27 15:30:49 +0200

    update debian image to 12.11.0

--- iso/scripts/generate_dappnode_iso_debian.sh
+++ iso/scripts/generate_dappnode_iso_debian.sh
@@ -5,11 +5,11 @@ SCRIPTS_DIR=$(dirname "${BASH_SOURCE[0]}")
 
 source ${SCRIPTS_DIR}/common_iso_generation.sh
 
-# Source = https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/debian-12.10.0-amd64-netinst.iso
-BASE_ISO_NAME="debian-12.10.0-amd64-netinst.iso"
+# Source = https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/debian-12.11.0-amd64-netinst.iso
+BASE_ISO_NAME="debian-12.11.0-amd64-netinst.iso"
 BASE_ISO_PATH="/images/${BASE_ISO_NAME}"
 BASE_ISO_URL="https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/${BASE_ISO_NAME}"
-BASE_ISO_SHASUM="ee8d8579128977d7dc39d48f43aec5ab06b7f09e1f40a9d98f2a9d149221704a  ${BASE_ISO_PATH}"
+BASE_ISO_SHASUM="30ca12a15cae6a1033e03ad59eb7f66a6d5a258dcf27acd115c2bd42d22640e8  ${BASE_ISO_PATH}"
 
 DAPPNODE_ISO_NAME="${DAPPNODE_ISO_PREFIX}${BASE_ISO_NAME}"
 DAPPNODE_ISO_PATH="/images/${DAPPNODE_ISO_NAME}"
