f0718d003db63866909ec269084f233877141a69
Marketen <marcfont12@gmail.com>
2026-08-20 13:03:23 +0200

    fix unattended usb tests

--- scripts/check-disks.sh
+++ scripts/check-disks.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-#
-# This is run by d-i before the partman step (e.g. d-i partman/early_command)
+# Selects non-USB disks for Debian partitioning before the partman step.
+# It gives GRUB the same targets so the installer USB is never selected.
 
 USBDEV=$(list-devices usb-partition | sed "s/\(.*\)./\1/" | sort -u | head -1 );
 if [ -z "${USBDEV}" ]; then
@@ -12,4 +12,5 @@ for DISK in ${DEVICE}; do
     DISKS="${DISKS} ${DISK}";
 done;
 DISKS=$(echo "${DISKS}" | sed "s/^ //g");
-debconf-set partman-auto/disk "$DISKS"; 
+debconf-set partman-auto/disk "$DISKS";
+debconf-set grub-installer/bootdev "$DISKS";
